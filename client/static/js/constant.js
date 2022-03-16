07fed7a9cc3f734163fbbc0971d8a9859d4138e1
happyGyu <happyboyto.dev@gmail.com>
2022-03-16 16:27:45 +0900

    docs: PR readme 작성

--- client/static/js/constant.js
+++ client/static/js/constant.js
@@ -1,4 +1,3 @@
-const URL = "https://coupang-alan.herokuapp.com/"; // "http://localhost:3000/"
-export const serverURL = `https://coupang-alan.herokuapp.com/`;
+export const serverURL = "http://localhost:3000/";
 export const initialCarouselIdx = 0;
 export const autoSlideInterval = 2000;

093a1620c35ed1a14c7edd4a00476e963173d382
happyGyu <happyboyto.dev@gmail.com>
2022-03-16 15:06:36 +0900

    chore: 헤로쿠 에러 수정...

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import path from "path";
 import carouselData from "./data/carousel.json" assert { type: "json" };
-import { searchRouter } from "./routers/searchRouter.js";
+// import { searchRouter } from "./routers/searchRouter.js";
 
 const app = express();
 const __dirname = path.resolve();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, "client", "static")));
-app.use("/search", searchRouter);
+// app.use("/search", searchRouter);
 app.get("/carousel", (req, res) => {
   res.json(carouselData);
 });
--- server/routers/searchRouter.js
+++ server/routers/searchRouter.js
@@ -1,7 +1,7 @@
-import express from "express";
-import searchCategory from "../data/searchCategory.json" assert { type: json };
-export const searchRouter = express.Router();
+// import express from "express";
+// import searchCategory from "../data/searchCategory.json" assert { type: json };
+// export const searchRouter = express.Router();
 
-searchRouter.get("/category", (req, res) => {
-  res.json(searchCategory);
-});
+// searchRouter.get("/category", (req, res) => {
+//   res.json(searchCategory);
+// });

5e2eb33cc2b0974797cf410d9f4a842f96600545
happyGyu <happyboyto.dev@gmail.com>
2022-03-16 14:40:00 +0900

    build: URL을 헤로쿠로 수정

--- client/static/js/constant.js
+++ client/static/js/constant.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3000;
-export const serverURL = `http://localhost:${PORT}/`;
+const URL = "https://coupang-alan.herokuapp.com/"; // "http://localhost:3000/"
+export const serverURL = `https://coupang-alan.herokuapp.com/`;
 export const initialCarouselIdx = 0;
 export const autoSlideInterval = 2000;

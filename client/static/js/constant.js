a12e37e2790454ce2fd5d7f3fa5253b2a01453cc
happyGyu <happyboyto.dev@gmail.com>
2022-03-16 14:57:40 +0900

    build: json import시 assert문제 수정

--- client/static/js/constant.js
+++ client/static/js/constant.js
@@ -1,4 +1,4 @@
-const URL = "https://coupang-alan.herokuapp.com/"; // "http://localhost:3000/"
-export const serverURL = `https://coupang-alan.herokuapp.com/`;
+//const URL = "https://coupang-alan.herokuapp.com/"; "http://localhost:3000/"
+export const serverURL = "https://coupang-alan.herokuapp.com/";
 export const initialCarouselIdx = 0;
 export const autoSlideInterval = 2000;

4a4f46c446dc5dff637015d16ea0edca6b5966b2
happyGyu <happyboyto.dev@gmail.com>
2022-03-16 14:26:19 +0900

    build: 헤로쿠 사용가능하도록 명령어 수정 및 포트 수정

--- client/static/js/constant.js
+++ client/static/js/constant.js
@@ -1,4 +1,4 @@
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 export const serverURL = `http://localhost:${PORT}/`;
 export const initialCarouselIdx = 0;
 export const autoSlideInterval = 2000;

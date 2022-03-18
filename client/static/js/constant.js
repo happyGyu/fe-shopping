96b56161a6e8b7184f38552c4c483e7b120f65e0
happyGyu <happyboyto.dev@gmail.com>
2022-03-18 19:16:31 +0900

    build: 헤로쿠 오류 수정

--- client/static/js/constant.js
+++ client/static/js/constant.js
@@ -1,4 +1,4 @@
-export const serverURL = "http://localhost:3000/";
+export const serverURL = "https://alpang.herokuapp.com/";
 export const initialCarouselIdx = 0;
 export const autoSlideInterval = 2000;
 export const initialSearchCategory = "전체";
